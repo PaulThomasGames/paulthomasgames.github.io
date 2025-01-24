272529aefab4c52bc44dbe12538376bca99c8058
Paul Thomas <paulthomashorror@gmail.com>
2025-01-23 19:44:51 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@ function openURL (url) {
     window.open("https://" + url, '_blank').focus();
 }
 
+const rotations = [0, 0, 90, 270];
+
 window.onload = (event) => {
+    var randomRotSelection = Math.floor(Math.random() * rotations.length)
+    document.documentElement.style.filter = `hue-rotate(${rotations[randomRotSelection]}deg)`;
     document.getElementById("p_intro").innerHTML = `Hello there, my name is Paul Thomas and I'm ${moment().diff('2007-07-08', 'years')} years old.<br>I design video games and other digital media.`;
     if($(window).width() < $(window).height()) {
         document.getElementById("box_main").style.display = "none";
